d43f1b18bd9a56e0b34eba148fea5963790f538c
MysteriousDrD <david.fennell.df@gmail.com>
2013-12-06 16:17:21 +0000

    skeleton code for handling input

---
+++ node.rb
@@ -0,0 +1,41 @@
+class Node
+  def handleInput
+    bar = "ROUTING_INFO" #this will be read in from other nodes later
+
+    case bar
+      when "JOINING_NETWORK"
+        puts bar
+
+      when "JOINING_NETWORK_RELAY"
+        puts bar
+
+      when "ROUTING_INFO"
+        puts bar
+
+      when "LEAVING_NETWORK"
+        puts bar
+
+      when "INDEX"
+        puts bar
+
+      when "SEARCH"
+        puts bar
+
+      when "SEARCH RESPONSE"
+        puts bar
+
+      when "PING"
+        puts bar
+
+      when "ACK"
+        puts bar
+
+      else
+        puts "invalid message"
+    end
+
+  end
+end
+
+nd = Node.new
+nd.handleInput
